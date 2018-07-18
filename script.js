79cf880075c94945dae1fd15f94ee778d4e823d9
amulinge <41160855+amulinge@users.noreply.github.com>
2018-07-18 14:14:36 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,23 @@
+$("#mole1").click(function(){
+    $("#mole1").hide();
+});
+    
+$("#moleTwo").click(function(){
+    $("#moleTwo").hide();
+}); 
+    
+$("#thirdMole").click(function(){
+    $("#thirdMole").hide();
+});
+    
+$("#mole1").click(function(){
+    $("#mole1").show();
+});
+
+$("#moleTwo").click(function(){
+    $("#mole1").show();
+});
+
+$("#thirdMole").click(function(){
+    $("#mole1").show();
+});
